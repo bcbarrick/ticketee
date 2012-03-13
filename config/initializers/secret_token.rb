3a1d2240aae528a4b3cf5d7c87eb589024176715
Brian Barrick <bcbarrick@comcast.net>
2012-03-12 23:11:36 -0700

    Generated the Rails 3 application

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ticketee::Application.config.secret_token = 'a80d234198f4769b4ba3a1fc97abcc29d5f136975fbbc704c6cdaabaabb702061f7ab770462706d41dc95b7c330988f847caf9a222b0414f6e172077ac46078f'
