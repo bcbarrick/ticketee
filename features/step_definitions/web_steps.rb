bd9f1c08c100d0560c3430ff69dd3b0da1a8488a
Brian Barrick <bcbarrick@comcast.net>
2012-03-13 19:27:26 -0700

    Added title functionality for show page

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -16,11 +16,20 @@ When /^(?:|I )press "([^"]*)"$/ do |button|
   click_button(button)
 end
 
-Then /^(?:|I )should see "([^"]*)"$/ do |text|
+Then /^(?:|I )should see "([^\"]*)"$/ do |text|
   if page.respond_to? :should
     page.should have_content(text)
   else
     assert page.has_content?(text)
+  end
+end
+
+Then /^(?:|I )should be on (.+)$/ do |page_name|
+  current_path = URI.parse(current_url).path
+  if current_path.respond_to? :should
+    current_path.should == path_to(page_name)
+  else
+    assert_equal path_to(page_name), current_path
   end
 end
 
