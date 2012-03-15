addc7e4ebdef84712e33d34b4f091c1450b36ba7
Brian Barrick <bcbarrick@comcast.net>
2012-03-15 15:13:14 -0700

    Implemented features for displaying a list of relevant tickets for projects and viewing particular tickets

--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -1,4 +1,17 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "paths"))
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "support", "selectors"))
+
+module WithinHelpers
+  def with_scope(locator)
+    locator ? within(*selector_for(locator)) { yield } : yield
+  end
+end
+World(WithinHelpers)
+
+# Single-line step scoper
+When /^(.*) within ([^:]+)$/ do |the_step, parent|
+  with_scope(parent) { step the_step }
+end
 
 Given /^(?:|I )am on (.+)$/ do |page_name|
   visit path_to(page_name)
