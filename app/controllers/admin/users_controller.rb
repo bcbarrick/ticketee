aafaf786c0f50adb2c056f92992245a42fd52961
Brian Barrick <bcbarrick@comcast.net>
2012-03-17 00:33:14 -0700

    Added the ability to create admin users through the admin backend

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -8,7 +8,11 @@ def new
 end
 
 def create
+  # extract the admin flag, as we cannot mass-assign this param when
+  # calling new (it is not attr_accessible)
+  admin =  params[:user].delete("admin") == "1"
   @user = User.new(params[:user])
+  @user.admin = admin
   if @user.save
     flash[:notice] = "User has been created."
     redirect_to admin_users_path
--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -25,6 +25,10 @@ When /^(?:|I )fill in "([^\"]*)" with "([^\"]*)"$/ do |field, value|
   fill_in(field, :with => value)
 end
 
+When /^(?:|I )check "([^\"]*)"$/ do |field|
+  check(field)
+end
+
 When /^(?:|I )press "([^\"]*)"$/ do |button|
   click_button(button)
 end
