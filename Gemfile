0a2148a000cfde1bd58dd1a92c38314d26068459
Brian Barrick <bcbarrick@comcast.net>
2012-03-26 00:17:15 -0700

    Added the ability to attach a file to a ticket

--- Gemfile
+++ Gemfile
@@ -25,6 +25,8 @@ gem 'jquery-rails'
 gem 'devise'
 gem 'cancan'
 
+gem 'paperclip'
+
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
 
--- features/step_definitions/web_steps.rb
+++ features/step_definitions/web_steps.rb
@@ -33,6 +33,10 @@ When /^(?:|I )press "([^\"]*)"$/ do |button|
   click_button(button)
 end
 
+When /^(?:|I )attach the file "([^\"]*)" to "([^\"]*)"$/ do |path, field|
+  attach_file(field, File.expand_path(path))
+end
+
 Then /^(?:|I )should see "([^\"]*)"$/ do |text|
   if page.respond_to? :should
     page.should have_content(text)
